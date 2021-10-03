3ef39483381d25d0853d4ffbd4d34b4759d2cb72
JiHye Kim <crk33333@gmail.com>
2021-10-03 12:20:38 +0900

    express 상세보기 구현

--- Nodejs-master/main.js
+++ Nodejs-master/main.js
@@ -1,12 +1,13 @@
 
 const express = require('express')
-const app = express()
+const app = express();
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var sanitizeHtml = require('sanitize-html');
+var path = require('path');
 
 
 const port = 3000
@@ -33,13 +34,36 @@ app.get('/',function(request, response){
   request.params = {'pageId' : 'HTML' } 이 된다.
 */
 app.get('/page/:pageId',function(request, response){
-  return response.send(request.params);
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update?id=${sanitizedTitle}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    });
+  });
 
 
 });
 
+/*
+app.listen(port,function(){
 
-
+})
+*/
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
@@ -58,41 +82,10 @@ var app = http.createServer(function(request,response){
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
-      if(queryData.id === undefined){
-        fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
+      if(queryData.id === undefined){ 
+        //구현완료
       } else {
-        fs.readdir('./data', function(error, filelist){
-          var filteredId = path.parse(queryData.id).base;
-          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-            var title = queryData.id;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {
-              allowedTags:['h1']
-            });
-            var list = template.list(filelist);
-            var html = template.HTML(sanitizedTitle, list,
-              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-              ` <a href="/create">create</a>
-                <a href="/update?id=${sanitizedTitle}">update</a>
-                <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizedTitle}">
-                  <input type="submit" value="delete">
-                </form>`
-            );
-            response.writeHead(200);
-            response.end(html);
-          });
-        });
+       
       }
     } else if(pathname === '/create'){
       fs.readdir('./data', function(error, filelist){
--- node.js-mysql-1/lib/db.js
+++ node.js-mysql-1/lib/db.js
@@ -1,3 +1,6 @@
+
+//이러한 DB정보가 저장된 파일을 버전관리에 올리는 것은 아주 나쁜 방법임.
+//따라서 버전관리에 저장되는 파일은 db.template.js를 저장함
 var mysql = require('mysql');
 var db = mysql.createConnection({
     host:'localhost',
